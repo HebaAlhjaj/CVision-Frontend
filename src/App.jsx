081df994db351cab627baf27a081b5d41ee11d7c
Heba Alhjaj <alhjajalaa2003@gmail.com>
2026-03-08 22:53:23 +0300

    frontend setup + team member structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Login from "./features/auth/login/Login";
 import Signup from "./features/auth/signup/Signup";
+import MyProject from "./features/team-member/my-project/MyProject";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
+
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+
+        <Route path="/team-member/my-project" element={<MyProject />} />
       </Routes>
     </BrowserRouter>
   );
