597e2154bf821c932879529782d9ad06005bd11f
Heba Alhjaj <alhjajalaa2003@gmail.com>
2026-05-20 23:11:08 +0300

    Update join route and project share UI

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import TeamMemberLayout from "./features/team-member/TeamMemberLayout";
 import MyProject from "./features/team-member/my-project/MyProject";
 import UploadCV from "./features/team-member/upload-cv/UploadCV";
 import MySkills from "./features/team-member/my-skills/MySkills";
+import JoinProject from "./features/team-member/join-project/JoinProject";
 
 import ProjectManagerLayout from "./features/project-manager/ProjectManagerLayout";
 import PMMyProject from "./features/project-manager/my-project/MyProject";
@@ -14,7 +15,7 @@ import ProjectDetails from "./features/project-manager/project-details/ProjectDe
 import TeamMembers from "./features/project-manager/team-members/TeamMembers";
 import RoleMatch from "./features/project-manager/role-match/RoleMatch";
 import SkillGaps from "./features/project-manager/skill-gaps/SkillGaps";
-import JoinProject from "./features/team-member/join-project/JoinProject";
+
 export default function App() {
   return (
     <BrowserRouter>
@@ -23,6 +24,8 @@ export default function App() {
 
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+
+        <Route path="/join/:slug" element={<JoinProject />} />
 
         <Route path="/team-member" element={<TeamMemberLayout />}>
           <Route index element={<Navigate to="my-project" replace />} />
@@ -34,13 +37,11 @@ export default function App() {
         <Route path="/project-manager" element={<ProjectManagerLayout />}>
           <Route index element={<Navigate to="my-project" replace />} />
           <Route path="my-project" element={<PMMyProject />} />
-         <Route path="project-details" element={<ProjectDetails />} />
+          <Route path="project-details" element={<ProjectDetails />} />
           <Route path="team-members" element={<TeamMembers />} />
           <Route path="role-match" element={<RoleMatch />} />
           <Route path="skill-gaps" element={<SkillGaps />} />
         </Route>
-
-        <Route path="/join-project" element={<JoinProject />} />
       </Routes>
     </BrowserRouter>
   );
